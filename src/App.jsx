a50391d0fc44df10e036b978dbefbe30df11fc42
Arpit Trikha <arpit_trikha05@yahoo.com>
2023-08-22 22:09:08 -0400

    More fixes

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
+  const baseUrl = "/Portfolio";
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={baseUrl}>
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
